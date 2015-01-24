dcdb1e6dcd3e75a3064d35ec4b806e80a42aa667
zhyq0826 <zhyq0826@gmail.com>
2015-01-24 12:15:06 +0800

    fix log.py apply to many user case

--- turbo/log.py
+++ turbo/log.py
@@ -1,38 +1,45 @@
 
 import os
 import logging
+import logging.handlers
 
 from turbo.conf import app_config
 
+_level = app_config.get_log_level or logging.warning
+_formatter = logging.Formatter('%(levelname)s:%(asctime)s %(name)s:%(lineno)d:%(funcName)s %(message)s')
 
-formatter = logging.Formatter('%(levelname)s:%(asctime)s %(name)s:%(lineno)d:%(funcName)s %(message)s')
 
+def _init_file_logger(logger, level, log_path, log_size, log_count):
+    
+    level = _level if level is None else level
+    if level not in [logging.DEBUG, logging.ERROR, logging.NOTSET, logging.WARNING, logging.INFO]:
+        level = logging.DEBUG
 
-def init_file_logger(logger, log_path, log_size, log_count):
+    for h in logger.handlers:
+        if isinstance(h, logging.handlers.RotatingFileHandler):
+            if h.level == :
+                return
+
     fh = logging.handlers.RotatingFileHandler(log_path, maxBytes=log_size, backupCount=log_count)
     fh.setLevel(app_config.get_log_level)
-    fh.setFormatter(formatter)
+    fh.setFormatter(_formatter)
     logger.addHandler(fh)
     
 
-def init_logger(logger):
+def _init_stream_logger(logger):
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    ch.setFormatter(formatter)
+    ch.setFormatter(_formatter)
     logger.addHandler(ch)
 
 
-def getLogger(currfile=None, log_path=None, log_size=500*1024*1024, log_count=3):
+def getLogger(currfile=None, level=None, log_path=None, log_size=500*1024*1024, log_count=3):
+    # init logger first
+    logger = None
 
-    if not logging.root.handlers:
-        root_logger = logging.getLogger()
-        root_logger.setLevel(app_config.get_log_level)
-        init_logger(root_logger)
-        if log_path:
-            init_file_logger(root_logger, log_path, log_size, log_count)
-
+    # py module logger
     path = os.path.abspath(currfile)
-    if os.path.isfile(path):
+    if os.path.isfile(path):    
         file_name = os.path.basename(path)
         module_name = file_name[0:file_name.rfind('.py')]
         logger_name_list = [module_name]
@@ -52,18 +59,29 @@ def getLogger(currfile=None, log_path=None, log_size=500*1024*1024, log_count=3)
 
         logger_name_list.reverse()
 
-        return logging.getLogger('.'.join(logger_name_list))
+        logger = logging.getLogger('.'.join(logger_name_list))
 
-    if isinstance(currfile, basestring) and currfile.strip():
+    #normal logger
+    if isinstance(currfile, basestring) and currfile.strip():   
         logger = logging.getLogger(currfile)
         logger.setLevel(app_config.get_log_level)
+        logger = logging.getLogger(currfile)
 
-        if logger.handlers:
-            return logger
+    if not logger:
+        logger = logging.getLogger()
 
-        return logging.getLogger(currfile)
+    if logger == logging.root:
+        if not logger.handlers:
+            _init_stream_logger(logger)
 
-    return logging.getLogger()
+    level = _level if level is None else level = 
+
+    if log_path:
+        _init_file_logger(logger, level, log_path, log_size, log_count)
+
+    return logger
+
+
 
 # Logger objects for internal turbo use
 app_log = getLogger('turbo.app')
