7bc32f1e15de7604e555c6f8d85cfb04d2ff05fc
zhyq0826 <zhyq0826@gmail.com>
2015-01-27 17:28:14 +0800

    release 0.2.3

--- turbo/__init__.py
+++ turbo/__init__.py
@@ -25,5 +25,5 @@ from __future__ import absolute_import, division, print_function, with_statement
 # is zero for an official release, positive for a development branch,
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
-version = "0.2.2"
-version_info = (0, 2, 2, 0)
+version = "0.2.3"
+version_info = (0, 2, 3, 0)
