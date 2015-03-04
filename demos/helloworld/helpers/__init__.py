ba1df1b575bb5aa5b404da36bd6f8edbf5a27c9f
zhyq0826 <zhyq0826@gmail.com>
2015-03-04 16:37:19 +0800

    fix demo helper install error

--- demos/helloworld/helpers/__init__.py
+++ demos/helloworld/helpers/__init__.py
@@ -4,5 +4,4 @@ from settings import INSTALLED_HELPERS
 
 import turbo.helper
 
-for item in INSTALLED_HELPERS:
-    turbo.helper.install_helper(INSTALLED_HELPERS, globals())
+turbo.helper.install_helper(INSTALLED_HELPERS, globals())
