15d5541d9e973ae96cb98f4424748f87cde8b90d
zhyq0826 <zhyq0826@gmail.com>
2015-03-16 14:34:00 +0800

    release 0.2.9

--- turbo/__init__.py
+++ turbo/__init__.py
@@ -28,4 +28,4 @@ from .conf import app_config
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
 version = "0.2.9"
-version_info = (0, 2, 9, 100)
+version_info = (0, 2, 9, 0)
