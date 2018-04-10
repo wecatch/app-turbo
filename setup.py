4106a8ae6d9779ae84ec4036cced05ca0f510ace
zhaoyongqiang <zhyq0826@gmail.com>
2018-04-10 08:30:32 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -46,5 +46,14 @@ setup(
     packages=find_packages(),
     install_requires=install_requires,
     scripts=['turbo/bin/turbo-admin'],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
     **kwargs
 )
